074f7b47b7deedba5c491b0287efad2b3656b6d1
mostafa216 <athadalarab123@gmail.com>
2023-09-22 17:19:07 +0300

    add  dio helper

---
+++ lib/Core/Database/remote/DioHelper/end_points.dart
@@ -0,0 +1,23 @@
+// End Points for Auth
+const baseUrl = 'https://tasksapp.integration25.com/api/';
+const loginEndPoint = 'auth/login';
+const logoutEndPoint = 'auth/logout';
+
+//settings
+
+const getAllDepartmentEndpoint = 'department/index';
+const addDepartmentEndpoint = 'department/store';
+const getManagersEndPoint = 'user/show-managers';
+const updateDepartmentEndpoint = 'department/update/';
+const deleteDepartmentEndpoint = 'department/delete/';
+
+const addUserEndpoint = 'user/store';
+const updateUserEndpoint = 'user/update/';
+const getAllUsersEndpoint = 'user/index';
+const getAllEmployeeEndpoint = 'user/show-employees';
+const deleteUserEndpoint = 'user/delete/';
+
+const addTaskEndpoint = 'task/store';
+const deleteTaskEndpoint = 'task/delete/';
+const getAllTasksEndpoint = 'task/index';
+const updateTasksEndpoint = 'task/update/';
