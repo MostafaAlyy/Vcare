50cc68ce20e855a19f5eaf662274298a7a36fb60
mostafa216 <athadalarab123@gmail.com>
2023-09-22 18:44:14 +0300

    link api

--- lib/Core/Database/remote/DioHelper/end_points.dart
+++ lib/Core/Database/remote/DioHelper/end_points.dart
@@ -1,23 +1,3 @@
 // End Points for Auth
-const baseUrl = 'https://tasksapp.integration25.com/api/';
-const loginEndPoint = 'auth/login';
-const logoutEndPoint = 'auth/logout';
-
-//settings
-
-const getAllDepartmentEndpoint = 'department/index';
-const addDepartmentEndpoint = 'department/store';
-const getManagersEndPoint = 'user/show-managers';
-const updateDepartmentEndpoint = 'department/update/';
-const deleteDepartmentEndpoint = 'department/delete/';
-
-const addUserEndpoint = 'user/store';
-const updateUserEndpoint = 'user/update/';
-const getAllUsersEndpoint = 'user/index';
-const getAllEmployeeEndpoint = 'user/show-employees';
-const deleteUserEndpoint = 'user/delete/';
-
-const addTaskEndpoint = 'task/store';
-const deleteTaskEndpoint = 'task/delete/';
-const getAllTasksEndpoint = 'task/index';
-const updateTasksEndpoint = 'task/update/';
+const baseUrl = 'https://vcare.integration25.com/api/';
+const registerEndpoint = 'auth/register';
--- lib/Features/Auth/ViewModel/cubit/auth_state.dart
+++ lib/Features/Auth/ViewModel/cubit/auth_state.dart
@@ -4,3 +4,10 @@ part of 'auth_cubit.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class RegisterSuccessState extends AuthState {
+  final String message;
+  RegisterSuccessState(this.message);
+}
+
+final class RegisterErrorState extends AuthState {}
